e46668f6d3ae33d14134ed56aa0101548699c71c
farmer0010 <zmqkf16@naver.com>
2026-04-28 13:02:53 +0900

    cgi 헤더파일 작성

--- includes/Cgi.hpp
+++ includes/Cgi.hpp
@@ -1,14 +1,28 @@
 #ifndef CGI_HPP
 # define CGI_HPP
 
+#include "HttpRequest.hpp"
+#include <string>
+#include <vector>
+#include <map>
+
 class Cgi
 {
 	private:
-		// CGI 관련 멤버 변수 선언 (예: 환경 변수, 프로세스 ID 등)
+		HttpRequest request;
+		std::string script_path;
+
+		std::map<std::string, std::string> env_map;
+		char **envp;
+
+		void initEnv();
+		void mapToCharArrays();
+		void freeEnv();
+		
 	public:
-		Cgi();
+		Cgi(const HttpRequest& req, const std::string& path);
 		~Cgi();
 
-		// CGI 처리 메서드 선언 (예: execute, setEnvironment 등)
+		std::vector<char> execute();
 };
 #endif
